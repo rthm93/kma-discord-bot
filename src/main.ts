3f1bf39a60076843d35ce6c4bedc1d0e621fbcff
Ryan Teh Hoon Meng <srwog@live.com>
2022-12-17 00:29:20 +0800

    KMA free games bot

--- src/main.ts
+++ src/main.ts
@@ -1,34 +1,74 @@
-/**
- * Some predefined delay values (in milliseconds).
- */
-export enum Delays {
-  Short = 500,
-  Medium = 2000,
-  Long = 5000,
+import { Client, Events, GatewayIntentBits, TextChannel } from 'discord.js';
+import { getGames } from 'epic-free-games';
+import { format } from 'date-fns';
+
+const channelId = '719191346149195842';
+const botToken =
+  'NzE1MjM1MDg0NzMwNjMwMjI2.GjIGf9.h0acew8s2ETQ-m7palRsvoUFTEGXJUzODtKENw';
+
+// Create a new client instance
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+// When the client is ready, run this code (only once)
+// We use 'c' for the event parameter to keep it separate from the already defined 'client'
+client.once(Events.ClientReady, async (c) => {
+  console.log(`Bot Ready! Logged in as ${c.user.tag}`);
+  console.log(`Start interval...`);
+
+  await scalpEpicAndNotifyEveryone(c);
+
+  // setInterval(async () => {
+  //   await scalpEpicAndNotifyEveryone(c);
+  // }, 10000);
+});
+
+async function scalpEpicAndNotifyEveryone(c: Client<true>) {
+  try {
+    console.log(`Getting free games from epic`);
+    const { currentGames } = await getGames('MY' as any, true);
+
+    if (!currentGames || !currentGames.length) {
+      console.warn('No free games :(');
+      return;
+    }
+
+    const messages: Array<{ message: string; files: string[] }> = [];
+
+    for (let game of currentGames) {
+      const gameMessage = `${game.title}\n${game.description}`;
+      console.log(`Free game: ${gameMessage}`);
+      messages.push({
+        message: gameMessage,
+        files: game.keyImages
+          .filter((i) => i.type !== 'VaultClosed')
+          .map((i) => i.url),
+      });
+    }
+
+    if (messages && messages.length) {
+      const channel = c.channels.cache.get(channelId);
+
+      if (channel && channel instanceof TextChannel) {
+        const textChannel = channel as TextChannel;
+        await textChannel.send(
+          `**Epic Games Free Games Update on ${format(
+            new Date(2014, 1, 11),
+            'dd MMM yyyy',
+          )}**`,
+        );
+
+        for (let message of messages) {
+          textChannel.send({
+            content: message.message,
+            embeds: message.files.map((url) => ({ image: { url } })),
+          });
+        }
+      }
+    }
+  } catch (err) {
+    console.error(err);
+  }
 }
 
-/**
- * Returns a Promise<string> that resolves after a given time.
- *
- * @param {string} name - A name.
- * @param {number=} [delay=Delays.Medium] - A number of milliseconds to delay resolution of the Promise.
- * @returns {Promise<string>}
- */
-function delayedHello(
-  name: string,
-  delay: number = Delays.Medium,
-): Promise<string> {
-  return new Promise((resolve: (value?: string) => void) =>
-    setTimeout(() => resolve(`Hello, ${name}`), delay),
-  );
-}
-
-// Please see the comment in the .eslintrc.json file about the suppressed rule!
-// Below is an example of how to use ESLint errors suppression. You can read more
-// at https://eslint.org/docs/latest/user-guide/configuring/rules#disabling-rules
-
-// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-export async function greeter(name: any) { // eslint-disable-line @typescript-eslint/no-explicit-any
-  // The name parameter should be of type string. Any is used only to trigger the rule.
-  return await delayedHello(name, Delays.Long);
-}
+// Log in to Discord with your client's token
+client.login(botToken);
